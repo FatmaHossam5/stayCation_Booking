8d0a60c09dbfd88c260d7eb88c429c44d150d39f
FatmaHossam5 <eng.fatma.fateh@gmail.com>
2024-01-04 23:04:29 +0200

    SideBar

--- src/Components/shared/MasterLayout/MasterLayout.tsx
+++ src/Components/shared/MasterLayout/MasterLayout.tsx
@@ -1,23 +1,20 @@
 import { Box } from '@mui/material'
-import React from 'react'
 import SideBar from '../SideBar/SideBar'
 import NavBar from '../NavBar/NavBar'
 import { Outlet } from 'react-router-dom'
 
 export default function MasterLayout() {
+
   return (
     <>
-     <Box>
-    <NavBar/>
-    </Box>
-    <Box>
-    <SideBar/>
-    </Box>
-    <Box>
-    <Outlet/>
-    </Box>
-    
-    
+      <NavBar />
+      <Box height={80} />
+      <Box sx={{ display: "flex" }}>
+        <SideBar />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Outlet />
+        </Box>
+      </Box>
     </>
   )
 }
