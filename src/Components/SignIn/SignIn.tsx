9a91e2e6b804420d7e31f0872d6f4699befa9b04
rahma-nagy <rahmanagy11sep@gmail.com>
2024-01-04 03:13:49 -0800

    login (ui-validation-integration) done

--- src/Components/SignIn/SignIn.tsx
+++ src/Components/SignIn/SignIn.tsx
@@ -16,15 +16,17 @@ const {baseUrl}=useContext(AuthContext)
   return (
     <>
     <FormControl component="form"  onSubmit={handleSubmit(signIn)} >
-     <Grid container spacing={2} justifyContent="center" alignItems="center" sx={{bgcolor:"#ffffff"}}>
+  <Grid container spacing={2} sx={{bgcolor:"#ffffff",display:'flex', alignItems: 'center', justifyContent: 'center', height: '100vh' ,paddingLeft:"120px"}}>
   <Grid item xs={6}>
-  <Typography sx={{color:'#000000',mr: 2,display:"flex",justifyContent:"flex-start",fontFamily:"cairo",fontSize:'30px',fontWeight:500,marginBottom:'20px'}} >Sign in</Typography>
-  <Typography sx={{color:'#000000',mr: 2,display:"flex",justifyContent:"flex-start",fontFamily:"cairo",fontSize:'20px',fontWeight:300,marginBottom:'20px'}} >If you don’t have an account registerYou can..</Typography>
-  <Typography sx={{color:'#000000',mr: 2,display:"flex",justifyContent:"flex-start",fontFamily:"cairo",fontSize:'20px',fontWeight:300}} >Email Address</Typography>
-    <TextField {...register('email')} label="Email" type="email" margin="normal" fullWidth sx={{bgcolor:"#F5F6F8"}}/>
-    <Typography sx={{color:'#000000',mr: 2,display:"flex",justifyContent:"flex-start",fontFamily:"cairo",fontSize:'20px',fontWeight:300}} >Password</Typography>
-    <TextField {...register('password')} label="Password" type="password" margin="normal" fullWidth sx={{bgcolor:"#F5F6F8"}}/>
-    <Button type="submit" variant="contained" fullWidth sx={{bgcolor:'#3252DF', '&:hover': { bgcolor: '#2843CC' },marginTop:"60px",fontFamily:"cairo"}}>
+  <Typography sx={{color:'#000000',mr: 2,display:"flex",justifyContent:"flex-start",fontFamily:"Poppins",fontSize:'30px',fontWeight:500,marginBottom:'20px',}} >Sign in</Typography>
+  <Typography sx={{color:'#000000',mr: 2,display:"flex",justifyContent:"flex-start",fontFamily:"Poppins",fontSize:'20px',fontWeight:300,marginBottom:'20px'}} >If you don’t have an account registerYou can..</Typography>
+  <Typography sx={{color:'#000000',mr: 2,display:"flex",justifyContent:"flex-start",fontFamily:"Poppins",fontSize:'20px',fontWeight:300}} >Email Address</Typography>
+    <TextField {...register('email', { required: true, pattern: /^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/ })} label="Email" type="email" margin="normal" fullWidth sx={{bgcolor:"#F5F6F8",fontFamily:"Poppins",}}/>
+    {errors.email && errors.email.type === 'required' && (<Typography sx={{color:'red'}}>email is required</Typography>)}
+    <Typography sx={{color:'#000000',mr: 2,display:"flex",justifyContent:"flex-start",fontFamily:"Poppins",fontSize:'20px',fontWeight:300}} >Password</Typography>
+    <TextField {...register('password', { required: true })} label="Password" type="password" margin="normal" fullWidth sx={{bgcolor:"#F5F6F8",fontFamily:"Poppins",}}/>
+    {errors.password&&errors.password.type==='required'&&(<Typography sx={{color:"red"}}> password is required</Typography>)}
+    <Button type="submit" variant="contained" fullWidth sx={{bgcolor:'#3252DF', '&:hover': { bgcolor: '#2843CC' },marginTop:"60px",fontFamily:"Poppins",}}>
       Sign In
     </Button>
   </Grid>
