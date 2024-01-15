f2b803007204a2cf4320edac207675fd84066206
rahma-nagy <rahmanagy11sep@gmail.com>
2024-01-15 11:49:07 -0800

    footer done

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Footer from '../shared/Footer/Footer'
 
 export default function LandingPage() {
   return (
     <div>
-      landing
+      <Navbar/>
+      mmmmmmmmm
+      <Footer/>
     </div>
   )
 }
