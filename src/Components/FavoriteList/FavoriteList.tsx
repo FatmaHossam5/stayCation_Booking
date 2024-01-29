900a443e6d4a54e80c4ebf32a7ef9339ab2c1d83
rahma-nagy <rahmanagy11sep@gmail.com>
2024-01-29 04:41:46 -0800

    fix design

--- src/Components/FavoriteList/FavoriteList.tsx
+++ src/Components/FavoriteList/FavoriteList.tsx
@@ -6,7 +6,9 @@ import CardContent from "@mui/material/CardContent";
 import { AuthContext } from "../../Context/AuthContext";
 import axios from "axios";
 import { Style } from "@mui/icons-material";
-import { Skeleton } from "@mui/material";
+import { Skeleton, Container, Divider } from "@mui/material";
+import NavBar from "../shared/NavBar/NavBar";
+import Footer from "../shared/Footer/Footer";
 
 export default function FavoriteList() {
   const [favList, setFavList] = useState([]);
@@ -31,6 +33,7 @@ export default function FavoriteList() {
 
   return (
     <>
+    <NavBar/>
       <Typography
         sx={{
           fontFamily: "Poppins",
@@ -53,41 +56,67 @@ export default function FavoriteList() {
       >
         Your Rooms
       </Typography>
+<Container >
+  
 
-      <Grid container spacing={2}>
-      {favList?.length > 0 &&
-        Array.from({ length: 15 }).map((_, repeatIndex) => (
-          <Grid item key={repeatIndex} xs={12} sm={6} md={4} lg={4}>
-            <Card sx={{ boxShadow: 'none' }}>
-              <CardContent>
-                {favList[0]?.images && favList[0]?.images.length > 0 ? (
-                  <div className={Style.imageWrapper}>
-                    <img
-                      src={favList[0]?.images[0]}
-                      alt={`Image ${repeatIndex + 1}`}
-                      style={{ width: '70%', height: 'auto' }}
-                    />
-                    <div>
-                      <Typography variant="h6" className={Style.roomName}>
-                        {favList[0]?.roomNumber}
-                      </Typography>
-                    </div>
-                  </div>
-                ) : (
-                  <Skeleton
-                    variant="rectangular"
-                    width="70%"
-                    height="100%"
-                    animation="wave"
-                    style={{ backgroundColor: '#E0E0E0' }}
-                  />
-                )}
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-    </Grid>
-      
+      <Grid container spacing={1}>
+  {favList?.length > 0 &&
+    Array.from({ length: 15 }).map((_, repeatIndex) => (
+      <Grid item key={repeatIndex} xs={12} sm={6} md={4} lg={4}>
+        <Card sx={{ position: 'relative', overflow: 'hidden', borderRadius:5,width:'90%'}}>
+          {favList[0]?.images && favList[0]?.images.length > 0 ? (
+            <div className={Style.imageWrapper} >
+               <div
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  right: 0,
+                  width: '30%',
+                  backgroundColor: '#FF498B',
+                  padding: '8px',
+                }}
+              >
+                <Typography variant="h6" className={Style.roomName} style={{ color: '#fff',paddingLeft:'15px' }}>
+                  $ {favList[0]?.discount} 
+                </Typography>
+              </div>
+              <img
+                src={favList[0]?.images[0]}
+                alt={`Image ${repeatIndex + 1}`}
+                style={{ width: '100%', }}
+              />
+              <div
+                style={{
+                  position: 'absolute',
+                  bottom: 0,
+                  left: 0,
+                  width: '100%',
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                  padding: '8px',
+                }}
+              >
+                <Typography variant="h6" className={Style.roomName} style={{ color: '#fff',paddingLeft:'15px' }}>
+                  {favList[0]?.roomNumber}
+                </Typography>
+              </div>
+            </div>
+          ) : (
+            <Skeleton
+              variant="rectangular"
+              width="100%"
+              height="100%"
+              animation="wave"
+              style={{ backgroundColor: '#E0E0E0', borderRadius: '15px' }}
+            />
+          )}
+        </Card>
+      </Grid>
+    ))}
+</Grid>
+
+</Container>
+<br></br>
+<Footer />
     </>
 
 
